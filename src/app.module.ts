8f69fd0aa5c176035d37b5b8042ca5345291bde8
renato <renato.roessler@pro22.com>
2023-06-01 21:41:19 -0300

    feat: insert userEntity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local'],
+    }),
+    TypeOrmModule.forRoot({
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      type: 'postgres',
+      synchronize: true,
+      entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+    }),
+    UserModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,20 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dtos';
+import { UserService } from './user.service';
+import { UserEntity } from './interface/user.entity';
 
 @Controller('user')
 export class UserController {
+
+  constructor(private readonly userService: UserService) { }
+
   @Post()
-  async createUser(@Body() createUser: CreateUserDto) {
-    return { ...createUser, password: undefined };
+  async createUser(@Body() createUser: CreateUserDto): Promise<UserEntity> {
+    return this.userService.createUser(createUser);
+  }
+
+  @Get()
+  async getAllUsers(): Promise<UserEntity[]> {
+    return this.userService.getAllUsers();
   }
 }
