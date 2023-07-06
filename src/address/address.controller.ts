0ea8444788651f7c47bf88a73fc6f2a3d3247e5a
renato <renato.roessler@pro22.com>
2023-07-06 18:31:44 -0300

    feat: insert validation in jwt

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,10 +1,12 @@
 import { Body, Controller, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AddressService } from './address.service';
 import { CreateAddressDto } from './dtos/createAddress.dto';
+import { Roles } from 'src/decorators/roles.decorator';
+import { UserType } from 'src/user/enum/user-type.enum';
 
+@Roles(UserType.User)
 @Controller('address')
 export class AddressController {
-
     constructor(private readonly addressService: AddressService) { }
 
     @Post('/:userId')
