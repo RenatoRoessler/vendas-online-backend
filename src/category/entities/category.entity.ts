f29353c1f97908b82c2ea035e2c0d0342de7f045
renato <renato.roessler@pro22.com>
2023-07-20 09:41:22 -0300

    feat: create categoryEntity and productEntity

---
+++ src/category/entities/category.entity.ts
@@ -0,0 +1,21 @@
+import { ProductEntity } from "src/product/entities/product.entity";
+import { Column, CreateDateColumn, Entity, JoinColumn, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: 'category' })
+export class CategoryEntity {
+    @PrimaryGeneratedColumn('rowid')
+    id: number;
+
+    @Column({ name: 'name', nullable: false })
+    name: string;
+
+    @CreateDateColumn({ name: 'created_at' })
+    createdAt: Date;
+
+    @CreateDateColumn({ name: 'updated_at' })
+    updatedAt: Date;
+
+    @OneToMany(() => ProductEntity, (product: ProductEntity) => product.category)
+    products: ProductEntity;
+
+}
