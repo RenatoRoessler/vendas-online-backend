6392b03ee1f8fcec2ea78c9c2d4189145402ea3d
renato <renato.roessler@pro22.com>
2023-10-12 17:08:13 -0300

    feat: insert product in cart

--- src/cart-product/cart-product.module.ts
+++ src/cart-product/cart-product.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CartProductService } from './cart-product.service';
+import { CartProductEntity } from './entities/cart-product.entity';
 
-@Module({})
-export class CartProductModule {}
+@Module({
+  imports: [TypeOrmModule.forFeature([CartProductEntity])],
+  providers: [CartProductService],
+  exports: [CartProductService]
+})
+export class CartProductModule { }
--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -3,13 +3,15 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { CartEntity } from './entities/cart.entity';
 import { Repository } from 'typeorm';
 import { InsertCartDTO } from './dtos/insert-cart.dto';
+import { CartProductService } from '../cart-product/cart-product.service';
 
 @Injectable()
 export class CartService {
 
     constructor(
         @InjectRepository(CartEntity)
-        private readonly cartRepository: Repository<CartEntity>
+        private readonly cartRepository: Repository<CartEntity>,
+        private readonly cartProductService: CartProductService
     ) { }
 
     async verifyActiveCart(userId: number): Promise<CartEntity> {
@@ -32,9 +34,12 @@ export class CartService {
         });
     }
 
-    async insertProductInCart(insertCart: InsertCartDTO, userId: number): Promise<CartEntity> {
+    async insertProductInCart(insertCartDTO: InsertCartDTO, userId: number): Promise<CartEntity> {
         const cart = await this.verifyActiveCart(userId).catch(async () => this.createCart(userId));
+        await this.cartProductService.insertProductInCart(insertCartDTO, cart);
         return cart;
+
+
         // return await this.cartRepository.save({
         //     ...cart,
         //     cartProduct: [
