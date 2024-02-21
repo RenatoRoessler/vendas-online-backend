2c71aef94abfaa8613929782f7f7728735353fce
renato <renato.roessler@pro22.com>
2024-02-21 19:41:50 -0300

    feat: create payment entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,8 @@ import { CategoryModule } from './category/category.module';
 import { ProductModule } from './product/product.module';
 import { CartModule } from './cart/cart.module';
 import { CartProductModule } from './cart-product/cart-product.module';
+import { PaymentStatusModule } from './payment-status/payment-status.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
   imports: [
@@ -41,7 +43,9 @@ import { CartProductModule } from './cart-product/cart-product.module';
     CategoryModule,
     ProductModule,
     CartModule,
-    CartProductModule],
+    CartProductModule,
+    PaymentStatusModule,
+    PaymentModule],
   controllers: [],
   providers: [{
     provide: APP_GUARD,
---
+++ src/payment-status/payment-status.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { PaymentStatusService } from './payment-status.service';
+
+
+@Module({
+  providers: [PaymentStatusService]
+})
+export class PaymentStatusModule { }
